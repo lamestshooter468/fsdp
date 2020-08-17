4157c7936b2beb27205b95e31727b0e0a4a3a011
lamestshooter468 <64242503+lamestshooter468@users.noreply.github.com>
2020-08-17 10:06:53 +0800

    Add files via upload

--- models/Support/PayMoney.js
+++ models/Support/PayMoney.js
@@ -6,6 +6,9 @@ const Paymoney=db.define("donation",{
     email:{type:Sequelize.STRING},
     message:{type:Sequelize.STRING(250)},
     name:{type:Sequelize.STRING},
+    CNo:{type:Sequelize.STRING},
+    CExpDate:{type:Sequelize.INTEGER},
+    CSecNo:{type:Sequelize.INTEGER},
     dateDonated:{type:Sequelize.DATE}
 });
 module.exports=Paymoney;
